da68e34ce9ab7793388217b5de3379cc2286db33
AndyR <marlon_rivera95@hotmail.com>
2022-05-25 12:46:13 -0500

    accesorios

---
+++ js/compras-peluches.js
@@ -0,0 +1,142 @@
+document.querySelector("#submit7").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref7 = document.querySelector("#ref7").textContent;
+let r7 = document.querySelector("#r7").textContent;
+let precio7 = document.querySelector("#precio7").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Aleja Cool_*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref7}%0A%0A
+*Nombre del producto:*%0D
+${r7}%0A%0A
+*Precio:*%0D
+${precio7}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit8").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref8 = document.querySelector("#ref8").textContent;
+    let r8 = document.querySelector("#r8").textContent;
+    let precio8 = document.querySelector("#precio8").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Bienvenidos a Aleja Cool_*%0A%0A
+		*Tu compra:*%0A%0A
+		*Ref del producto:*%0D
+		${ref8}%0A%0A
+		*Nombre del producto:*%0D
+		${r8}%0A%0A
+		*Precio:*%0D
+		${precio8}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit9").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref9 = document.querySelector("#ref9").textContent;
+    let r9 = document.querySelector("#r9").textContent;
+    let precio9 = document.querySelector("#precio9").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Bienvenidos a Aleja Cool_*%0A%0A
+		*Tu compra:*%0A%0A
+		*Ref del producto:*%0D
+		${ref9}%0A%0A
+		*Nombre del producto:*%0D
+		${r9}%0A%0A
+		*Precio:*%0D
+		${precio9}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit10").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref10 = document.querySelector("#ref10").textContent;
+    let r10 = document.querySelector("#r10").textContent;
+    let precio10 = document.querySelector("#precio10").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Bienvenidos a Aleja Cool_*%0A%0A
+		*Tu compra:*%0A%0A
+		*Ref del producto:*%0D
+		${ref10}%0A%0A
+		*Nombre del producto:*%0D
+		${r10}%0A%0A
+		*Precio:*%0D
+		${precio10}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit11").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref11 = document.querySelector("#ref11").textContent;
+    let r11 = document.querySelector("#r11").textContent;
+    let precio11 = document.querySelector("#precio11").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Bienvenidos a Aleja Cool_*%0A%0A
+		*Tu compra:*%0A%0A
+		*Ref del producto:*%0D
+		${ref11}%0A%0A
+		*Nombre del producto:*%0D
+		${r11}%0A%0A
+		*Precio:*%0D
+		${precio11}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit12").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref12 = document.querySelector("#ref12").textContent;
+    let r12 = document.querySelector("#r12").textContent;
+    let precio12 = document.querySelector("#precio12").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Bienvenidos a Aleja Cool_*%0A%0A
+		*Tu compra:*%0A%0A
+		*Ref del producto:*%0D
+		${ref12}%0A%0A
+		*Nombre del producto:*%0D
+		${r12}%0A%0A
+		*Precio:*%0D
+		${precio12}%0A%0A`;
+
+    window.open(url);    
+});
